56ce70b943b6c1b54f5213d5696b36d9798ad3ba
alexaugustobr <alexaugustoti@gmail.com>
2026-04-23 10:02:19 -0300

    feat: EMS - 05.08.07

---
+++ src/main/java/com/algaworks/algashop/product/catalog/application/security/SecurityCheckApplicationService.java
@@ -0,0 +1,14 @@
+package com.algaworks.algashop.product.catalog.application.security;
+
+import java.util.UUID;
+
+public interface SecurityCheckApplicationService {
+
+    UUID getAuthenticatedUserId();
+
+    boolean isAuthenticated();
+
+    boolean isMachineAuthenticated();
+
+}
+
