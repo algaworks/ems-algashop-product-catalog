b5b18ccb510c44153fe34c2ffd78915a7b021af3
alexaugustobr <alexaugustoti@gmail.com>
2026-07-15 15:26:34 -0300

    EMS - 07.03.04 - Publish events

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/listener/product/ProductEventListener.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/listener/product/ProductEventListener.java
@@ -1,6 +1,8 @@
 package com.algaworks.algashop.product.catalog.infrastructure.listener.product;
 
+import com.algaworks.algashop.product.catalog.application.IntegrationEventPublisher;
 import com.algaworks.algashop.product.catalog.domain.model.product.*;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
@@ -8,42 +10,52 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class ProductEventListener {
+
+    private final IntegrationEventPublisher integrationEventPublisher;
 
     @EventListener(ProductPriceChangedEvent.class)
     @Async
     public void handle(ProductPriceChangedEvent event) {
         log.info("ProductPriceChangedEvent " + event);
+        integrationEventPublisher.send(event, "product-catalog.product.events");
     }
 
     @EventListener(ProductPlacedOnSaleEvent.class)
     public void handle(ProductPlacedOnSaleEvent event) {
         log.info("ProductPlacedOnSaleEvent " + event);
+        integrationEventPublisher.send(event, "product-catalog.product.events");
     }
 
     @EventListener(ProductAddedEvent.class)
     public void handle(ProductAddedEvent event) {
         log.info("ProductAddedEvent " + event);
+        integrationEventPublisher.send(event, "product-catalog.product.events");
     }
 
     @EventListener(ProductDelistedEvent.class)
     public void handle(ProductDelistedEvent event) {
         log.info("ProductDelistedEvent  " + event);
+        integrationEventPublisher.send(event, "product-catalog.product.events");
     }
 
     @EventListener(ProductListedEvent.class)
     public void handle(ProductListedEvent event) {
         log.info("ProductListedEvent " + event);
+        integrationEventPublisher.send(event, "product-catalog.product.events");
     }
 
     @EventListener(ProductRestockedEvent.class)
     public void handle(ProductRestockedEvent event) {
         log.info("ProductRestockedEvent  " + event);
+        integrationEventPublisher.send(event, "product-catalog.product.events");
     }
 
     @EventListener(ProductSoldOutEvent.class)
     public void handle(ProductSoldOutEvent event) {
         log.info("ProductSoldOutEvent " + event);
+        integrationEventPublisher.send(event, "product-catalog.product.events");
     }
 
 }
